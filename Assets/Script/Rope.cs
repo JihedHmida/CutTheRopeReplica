a030e87f381110f076692e5fb896e77d95da0a49
JihedHmida <loveaflo@gmail.com>
2017-06-08 15:46:49 +0100

    Scene

---
+++ Assets/Script/Rope.cs
@@ -0,0 +1,25 @@
+﻿#region System Usage 
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+#endregion
+
+public class Rope : MonoBehaviour {
+
+	
+	void Start () 
+	{
+		
+		
+	}
+	
+	
+	void Update () 
+	{
+		
+		
+	}
+}
